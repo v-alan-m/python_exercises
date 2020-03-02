946c8ef2ad8fa7d6ce1a110ebe281e6be66e8d43
v-alan-m <muhunthanv9@gmail.com>
2020-03-02 22:27:03 +0000

    Solution to exercise_01.

---
+++ bubble_sort_alan.py
@@ -0,0 +1,12 @@
+def bubble_sort(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr)-1):
+            if arr[j] > arr[j+1]:
+                stored = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = stored
+
+if __name__ == "__main__":
+    arr = [8,9,4,5,6,7,2,3]
+    bubble_sort(arr)
+    print(arr)
