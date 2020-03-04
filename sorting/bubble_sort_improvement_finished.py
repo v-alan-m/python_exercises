3c87d79047e08cbdda5b3231704d67857e31f1c2
v-alan-m <muhunthanv9@gmail.com>
2020-03-04 19:55:04 +0000

    3 improvements to bubble sort.

---
+++ sorting/bubble_sort_improvement_finished.py
@@ -0,0 +1,39 @@
+def bubble_sort_improvement_finished(arr):
+
+    passes = 0
+    comparisions = 0
+    swaps = 0
+    changed = True
+
+    if changed:
+        for i in range(len(arr) - 1):
+            print(f"\n-------- Start: pass {i+1} ---------\n")
+            
+            for j in range(len(arr) - 1 - i):
+                comparisions += 1
+                print(f"Comparing: {arr[j]} and {arr[j+1]}")
+                changed = False
+
+                if arr[j] > arr[j+1]:
+                    swaps += 1
+                    
+                    stored = arr[j]
+                    arr[j] = arr[j+1]
+                    arr[j+1] = stored
+
+                    changed = True
+
+                    print(f"*Swapped: {arr[j]} and {arr[j+1]}")
+                    print(f"\n{arr}\n")
+
+            passes += 1
+
+    print(f"\n------------------------------------\n\nNumber of passes: {passes} \nNumber of comparisons: {comparisions} \nNumber of swaps: {swaps}")
+    print(f"\nSorted List: {arr}")
+
+
+if __name__ == "__main__":
+
+    arr = [8,9,4,5,6,7,2,3]
+
+    bubble_sort_improvement_finished(arr)
