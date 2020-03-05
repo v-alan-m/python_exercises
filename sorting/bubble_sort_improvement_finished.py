d1d7266575e9e7467c340bf233c8464c76d14174
v-alan-m <muhunthanv9@gmail.com>
2020-03-05 20:32:11 +0000

    Included test cases, with improved sorting performance summary.

--- sorting/bubble_sort_improvement_finished.py
+++ sorting/bubble_sort_improvement_finished.py
@@ -35,9 +35,33 @@ def bubble_sort_finished(arr):
     print(f"\nNumber of passes: {passes} \nNumber of comparisons: {comparisions} \nNumber of swaps: {swaps}")
     print(f"\nSorted List: {arr}")
 
+    return passes, comparisions, swaps
+
 
 if __name__ == "__main__":
 
-    arr = [8,9,4,5,6,7,2,3]
+    test_cases = [[3, 1, 0, 4, 2], [3, 1, 2, 0, 5, 7, 9, 4, 8, 6], [4, 3, 2, 1], [8, 7, 6, 5, 4, 3, 2, 1]]
 
-    bubble_sort_finished(arr)
+    list_n = 0
+    
+    results = {}
+
+    for test in test_cases:
+        
+        list_n += 1
+
+        passes, comparisions, swaps = bubble_sort_finished(test)
+
+        results[(list_n, 'Passes')] = passes
+        results[(list_n, 'Comparisions')] = comparisions
+        results[(list_n, 'Swaps' )] = swaps
+
+    inspect = (int(len(results)/3))
+    print('\nP = Passes')
+    print('C = Comparisons')
+    print('S = Swaps\n')
+    print('           P', '   C', '   S')
+    print('        ----------------')
+
+    for list_n in range(1, inspect + 1):
+        print('List', list_n, ":", " ",results[(list_n, 'Passes')], " ",results[(list_n, 'Comparisions')], " ",results[(list_n, 'Swaps' )])
