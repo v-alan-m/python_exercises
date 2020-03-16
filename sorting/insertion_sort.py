a2c341fa9ff5db059412dad83cff5e391dffae30
v-alan-m <muhunthanv9@gmail.com>
2020-03-16 05:33:27 +0000

    Instrumentation added to functions.

--- sorting/insertion_sort.py
+++ sorting/insertion_sort.py
@@ -1,21 +1,51 @@
 def insertion_sort(arr):
     
+    passes = 0
+    comparisions = 0
+    swaps = 0
+
+
     for i in range(1, len(arr)):
+        passes += 1
         current_value = arr[i]
         position = i
         
         while position > 0 and arr[position - 1] > current_value:
-            
+            comparisions += 1
             arr[position] = arr[position - 1]
             position -= 1
             
         arr[position] = current_value
+        swaps += 1
     
-    print(arr)
+    return passes, comparisions, swaps
 
 
 if __name__ == "__main__":
 
-    arr = [9, 8, 7, 6, 5, 4, 3, 2, 1]
+    test_cases = [[3, 1, 0, 4, 2], [3, 1, 2, 0, 5, 7, 9, 4, 8, 6], [4, 3, 2, 1], [8, 7, 6, 5, 4, 3, 2, 1]]
 
-    insertion_sort(arr)
+    list_n = 0
+    
+    results = {}
+
+    for test in test_cases:
+        
+        list_n += 1
+
+        passes, comparisions, swaps = insertion_sort(test)
+
+        results[(list_n, 'Passes')] = passes
+        results[(list_n, 'Comparisions')] = comparisions
+        results[(list_n, 'Swaps' )] = swaps
+
+
+    inspect = (int(len(results)/3))
+    print('\nP = Passes')
+    print('C = Comparisons')
+    print('S = Swaps\n')
+    print('           P', '   C', '   S')
+    print('        ----------------')
+
+    for list_n in range(1, inspect + 1):
+        print('List', list_n, ":", " ",results[(list_n, 'Passes')], " ",results[(list_n, 'Comparisions')], " ",results[(list_n, 'Swaps' )])
--- sorting/selection_sort.py
+++ sorting/selection_sort.py
@@ -1,21 +1,53 @@
 def selection_sort(arr):
-        
+    
+    passes = 0
+    comparisions = 0
+    swaps = 0
+
     for sorted_n in range(len(arr) - 1, 0, -1):
+        passes += 1
         current_max = 0
         
         for unsorted_n in range(1, sorted_n + 1):
+            comparisions += 1
+
             if arr[unsorted_n] > arr[current_max]:
+                swaps += 1
                 current_max = unsorted_n
         
         temp = arr[sorted_n]
         arr[sorted_n] = arr[current_max]
         arr[current_max] = temp
         
-    print(arr)
+
+    return passes, comparisions, swaps
 
 
 if __name__ == "__main__":
 
-    arr = [9, 8, 7, 6, 5, 4, 3, 2, 1]
+    test_cases = [[3, 1, 0, 4, 2], [3, 1, 2, 0, 5, 7, 9, 4, 8, 6], [4, 3, 2, 1], [8, 7, 6, 5, 4, 3, 2, 1]]
 
-    selection_sort(arr)
+    list_n = 0
+    
+    results = {}
+
+    for test in test_cases:
+        
+        list_n += 1
+
+        passes, comparisions, swaps = selection_sort(test)
+
+        results[(list_n, 'Passes')] = passes
+        results[(list_n, 'Comparisions')] = comparisions
+        results[(list_n, 'Swaps' )] = swaps
+
+
+    inspect = (int(len(results)/3))
+    print('\nP = Passes')
+    print('C = Comparisons')
+    print('S = Swaps\n')
+    print('           P', '   C', '   S')
+    print('        ----------------')
+
+    for list_n in range(1, inspect + 1):
+        print('List', list_n, ":", " ",results[(list_n, 'Passes')], " ",results[(list_n, 'Comparisions')], " ",results[(list_n, 'Swaps' )])
